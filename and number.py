8c9493105b2a1bad28ac61f6c6dcba7c761b72ec
Dmytro200 <dimosinko@gmail.com>
2022-04-14 10:08:54 +0300

    p

---
+++ and number.py
@@ -0,0 +1,15 @@
+age_0 = 22
+age_1 = 18
+print( age_0 >= 21 and age_1 >= 21)
+
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21)
+
+age_0 = 22
+age_1 = 18
+print( age_0 >= 21 or age_1 >= 21)
+
+age_0 = 18
+age_1 = 18
+print( age_0 >= 21 or age_1 >= 21)
+
