2d020a7cb95ea4190a5dbc760384dcf8260a22d3
Dmytro200 <dimosinko@gmail.com>
2022-04-14 16:10:15 +0300

    123

---
+++ foods.py
@@ -0,0 +1,21 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+for my_food in my_foods:
+    print(f"My favourite foods are:{my_food}.")
+
+for friend_food in friend_foods:
+    print(f"My friend's favourite foods are:{friend_food}.")
+
+
+
+
+
+
+
+
+
+
