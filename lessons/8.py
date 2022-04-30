3bb7d7467339b976d9a10408a214a83a51db4665
Dmytro200 <dimosinko@gmail.com>
2022-04-30 15:37:22 +0300

    8

---
+++ lessons/8.py
@@ -0,0 +1,60 @@
+text1 = input("Виберіть одну з операцій (+, -, *, /, +++):")
+
+numbers = []
+total = 0
+
+if text1 == '+++':
+
+    text = None
+    while text != '':
+        text = input("Введіть число и напишите Enter (якщо бажаете завершити, просто напишите Enter):")
+        if text != '':
+            numbers.append(int(text))
+    print(sum(numbers))
+
+elif text1 == '+':
+    text2 = input("Введіть перше число:")
+
+    text3 = input("Введіть друге число:")
+
+    print(f"{int(text2)}+{int(text3)}={int(text2) + int(text3)}")
+
+elif text1 == '-':
+    text2 = input("Введіть перше число:")
+
+    text3 = input("Введіть друге число:")
+
+    print(f"{int(text2)}-{int(text3)}={int(text2) - int(text3)}")
+
+elif text1 == '*':
+    text2 = input("Введіть перше число:")
+
+    text3 = input("Введіть друге число:")
+
+    print(f"{int(text2)}*{int(text3)}={int(text2) * int(text3)}")
+
+elif text1 == '/':
+    text2 = input("Введіть перше число:")
+
+    text3 = input("Введіть друге число:")
+
+    if int(text3) == 0:
+         try:
+             text3 = 0
+         except IndexError:
+             text3 = ''
+         finally:
+             print('На ноль делить нельзя')
+
+    else:
+         print(f"{int(text2)}/{int(text3)}={int(text2) / int(text3)}")
+
+else:
+    print("Невірний символ(((")
+
+
+# try:
+#
+# except IndexError:
+#
+# elif
